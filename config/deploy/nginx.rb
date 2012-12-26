df736f3c666028367d068161dae73643f5e25a18
Lizhe <me@lizhe.name>
2012-12-26 16:56:41 +0800

    bugfix for nginx recipie

--- config/deploy/nginx.rb
+++ config/deploy/nginx.rb
@@ -1,20 +1,17 @@
 namespace :nginx do
   task :relink_nginx_config, :roles => :web do
     template "nginx.erb", "#{shared_path}/config/nginx.conf"
-    run "ln -s #{shared_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    run "if [ if /etc/nginx/sites-enalbed/#{application} ]; then #{sudo} rm /etc/nginx/sites-enabled/#{application}; fi"
+    run "#{sudo} ln -s #{shared_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
   end
 
-  task :start, :roles => :web, :except => { :no_release => true } do
-    run "#{sudo} service nginx start"
+  %w{start stop restart}.each do |command|
+    task command , :roles => :web do
+      run "#{sudo} service nginx #{command}"
+    end
   end
 
-  task :stop, :roles => :web, :except => { :no_release => true } do
-    run "#{sudo} service nginx stop"
-  end
-
-  task :restart, :roles => :web, :except => { :no_release => true } do
-    run "#{sudo} service nginx restart"
-  end
 end
 
 before "nginx:restart", "nginx:relink_nginx_config"
+before "nginx:start", "nginx:relink_nginx_config"
