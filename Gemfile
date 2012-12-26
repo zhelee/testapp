182c6d60338aadd6cff43d51a07e4ce1fcd02bb8
Li Zhe <mark@funky-factory-2.2bu.ro>
2012-12-26 13:40:50 +0800

    init commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :assets, :development do
+  gem 'therubyracer', '0.10.2'
+end
+
+gem 'jquery-rails'
+
+# Use unicorn as the app server
+gem 'unicorn'
+
+# Deploy with Capistrano
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'db1007553c670f7d89f97d76a0ea774e33d230573bb6108a6eefdad79a0ab957ae22a27c9a312c59d64bd3adb3f2c86a3af9f3563cbc7be0a8230807efe6cd52'
