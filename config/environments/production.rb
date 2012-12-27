4a1399c1a01d3b71800c9be46eab3c14211c43e5
Lizhe <me@lizhe.name>
2012-12-27 17:19:13 +0800

    add production exception notification settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,10 @@ Testapp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.middleware.use ExceptionNotifier,
+    sender_address: 'exception_notification@testapp.com',
+    exception_recipients: 'mark.geek.lee@gmail.com'
+
+  config.action_mailer.delivery_method = :sendmail
 end
